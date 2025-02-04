a5c9f3b46c9113b87fba68a8d19db28c0fd54503
Vedurupaka Venkata Satya Akhil <86138424+SatyaAkhil@users.noreply.github.com>
2025-02-04 13:47:13 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,9 +1,6 @@
 import instaloader
-
 loader = instaloader.Instaloader()
-
 post_url = "https://www.instagram.com/p/DDydlR_SYQG/?igsh=NXMwNmtqd2cybml1"  
 post_id = post_url.split("/")[-2]  
-
 loader.download_post(instaloader.Post.from_shortcode(loader.context, post_id), target="downloads")
 print("Download complete!")
